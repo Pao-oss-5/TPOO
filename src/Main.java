fc73eb83931ae096f3ee2f09fb16c6f311886252
Paola <l24120372@morelia.tecnm.mx>
2024-10-31 17:32:18 -0600

    tarea 7 terminada

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("***** BIENVENIDO A SEGUROS *****");
+        System.out.println("\nSEGURO AUTOS");
+        SeguroAuto seguroAuto1 = new SeguroAuto("Paola Garcia", 1650.00, 26);
+        seguroAuto1.calcularPrima();
+        seguroAuto1.detallesSeguro();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        SeguroAuto seguroAuto2 = new SeguroAuto("Andres Salamanca", 10000.00, 29);
+        seguroAuto2.calcularPrima();
+        seguroAuto2.detallesSeguro();
+
+        System.out.println("\nSEGURO HOGAR");
+        SeguroHogar seguroHogar1 = new SeguroHogar("Amy Rodriguez", 3620.00, 48, true);
+        seguroHogar1.calcularPrima();
+        seguroHogar1.detallesSeguro();
+
+        SeguroHogar seguroHogar2 = new SeguroHogar("Camila Hernandez", 5550.00, 46, false);
+        seguroHogar2.calcularPrima();
+        seguroHogar2.detallesSeguro();
+
+        System.out.println("\nSEGURO VIDA");
+        SeguroVida seguroVida1 = new SeguroVida("Eliseo Perez", 1960.00, 26);
+        seguroVida1.calcularPrima();
+        seguroVida1.detallesSeguro();
+
+        SeguroVida seguroVida2 = new SeguroVida("Daniela Montufar", 10000090.00, 65);
+        seguroVida2.calcularPrima();
+        seguroVida2.detallesSeguro();
+
     }
 }
