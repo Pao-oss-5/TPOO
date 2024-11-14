dea1bda67b8f6517e79150b77c9355bec0d08887
Paola <l24120372@morelia.tecnm.mx>
2024-11-13 22:31:06 -0600

    tarea terminada

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,61 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        int opcion=0;
+        while(opcion!=3){
+            System.out.println("Notas Pao :)");
+            System.out.println("1.Agregar tarea");
+            System.out.println("2.Lista de Tareas");
+            System.out.println("3.Salir");
+            try{
+                opcion=sc.nextInt();
+                sc.nextLine();
+                switch(opcion){
+                    case 1:
+                        System.out.println("Ingresa tu tarea");
+                        try (FileWriter writer = new FileWriter("ListDeTareas.txt", true);
+                             BufferedWriter bw = new BufferedWriter(writer)){
+                            System.out.print("Ingrese su tarea: ");
+                            String tarea = sc.nextLine();
+                            bw.write(tarea + "\n");
+                        }catch (IOException e){
+                            System.out.println("Error aal sobreescribir el archivo: " + e.getMessage());
+                        }
+                        break;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+                    case 2:
+                        System.out.println("Lista de tares: ");
+                        try (BufferedReader read = new BufferedReader(new FileReader("ListaDeTareas.txt"))) {
+                            String lectura;
+                            while ((lectura = read.readLine()) != null) {
+                                System.out.println(lectura);
+                            }
+                        } catch (IOException e){
+                            System.out.println("Error al leer el archivo :(" + e.getMessage());
+                        }
+                        break;
+
+                    case 3:
+                        System.out.print("Programa cerrado :/ ");
+                        break;
+
+                    default:
+                        System.out.println("Opcion invalida");
+
+
+                }
+
+            }catch(Exception e){
+                System.out.println("Ingrese un numero por favor");
+                sc.nextLine();
+
+            }
+            System.out.println();
         }
+
     }
+
 }
